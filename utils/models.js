b6059141835aa4eb58d5a30953a19f77af9bc059
Tris <gingertris1@gmail.com>
2022-10-23 18:19:30 +0100

    unnecessary file

--- utils/models.js
+++
@@ -1,9 +0,0 @@
-import Player from '../models/player.js';
-import Team from '../models/team.js';
-
-export default () => {
-    Team.hasMany(Player)
-    Team.hasOne(Player, {as: 'Captain', foreignKey: 'captainId'})
-    Player.belongsTo(Team)
-    return
-}
